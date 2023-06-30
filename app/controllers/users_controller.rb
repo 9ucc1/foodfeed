ed97959a5cf7157a82b3c6ad2a8cf130d2240342
Gucci Fan <gucc@umich.edu>
2023-06-30 19:43:18 -0400

    create user persist in database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        render json: user, status: :created
+    end
+
+    def index
+        render json: User.all
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 end
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Signup(){
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirmation, setPasswordConfirmation] = useState("")
+
+    function handleSubmit(e){
+        console.log("submit")
+        e.preventDefault()
+        fetch('/signup', {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                password_confirmation: passwordConfirmation
+            })
+        })
+        .then(r=>r.json())
+        .then(console.log("user created"))
+    }
+
     return (
         <>
-        <div>signup</div>
-        <form>
+        <div>Create a new account</div>
+        <form onSubmit={handleSubmit}>
             <label>Username:</label>
+            <input
+                type="text"
+                value={username}
+                onChange={e=>setUsername(e.target.value)}
+            />
+            <br/>
+            <label>Password:</label>
+            <input 
+                type="password"
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+            />
+            <br/>
+            <label>Confirm Password:</label>
+            <input 
+                type="password"
+                value={passwordConfirmation}
+                onChange={e=>setPasswordConfirmation(e.target.value)}
+            />
+            <br/>
+            <button type="submit">Sign Up</button>
         </form>
         </>
     )
