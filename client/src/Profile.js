df4715c88bc3a11069a41c376ae3769e4c21244c
Gucci Fan <gucc@umich.edu>
2023-08-16 10:47:04 -0400

    profile displays user's posts

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,14 +1,17 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {useParams, Link} from 'react-router-dom'
 import {UserContext} from './context/user'
+import {PostsContext} from './context/posts'
+import Post from './Post.js'
 
 
 function Profile(){
 
     const params = useParams()
     const {user} = useContext(UserContext)
+    const {posts} = useContext(PostsContext)
     const [profile, setProfile] = useState({image: "", profile: {display_name: ""}})
-    console.log(params)
+    //console.log(params)
 
     useEffect(()=>{
         fetch(`/profiles/${params.id}`)
@@ -16,6 +19,20 @@ function Profile(){
         .then(r=>setProfile(r))
     },[])
     console.log(profile)
+
+    /*function renderPosts(){
+        const profilePosts = posts.filter(post=>post.user_id == params.id)
+        profilePosts.map(post=><Post 
+            image_url = {post.image_url}
+            caption = {post.caption}
+            post_id = {post.id}
+            user_id = {post.user_id}
+            comments = {post.comments}
+        />)
+        console.log(posts)
+    }*/
+
+    const profilePosts = posts.filter(post=>post.user_id == params.id)
 
     return(
         <>
@@ -29,6 +46,13 @@ function Profile(){
             <div>{profile.profile.bio}</div>
             <h3>{profile.profile.display_name}'s posts</h3>
         </div>
+        {        profilePosts.map(post=><Post 
+            image_url = {post.image_url}
+            caption = {post.caption}
+            post_id = {post.id}
+            user_id = {post.user_id}
+            comments = {post.comments}
+        />)}
         </>
     )
 }
